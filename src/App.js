64faf9bd2f2c01e5330e0101adbad2ec040ff5a2
deepali-02 <deepali93.redekar@gmail.com>
2022-04-30 13:26:49 +0200

    import Team page in App.js and test the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+
+import { Team } from "./Page/Team";
 
 function App() {
   return (
     <div className="App">
-    
+      <Team />
     </div>
   );
 }
