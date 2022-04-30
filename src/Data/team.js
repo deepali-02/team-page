9973ead5fde973991637b943078ef76152b73498
deepali-02 <deepali93.redekar@gmail.com>
2022-04-30 13:09:35 +0200

    create new folder- "Data" to store team members information

---
+++ src/Data/team.js
@@ -0,0 +1,157 @@
+export const team = [
+    {
+      "name": "Jessika",
+      "role": "Interaction Design",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["salmon", "red"]
+    },
+    {
+      "name": "Kyra",
+      "role": "Project Manager",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["white", "gold"]
+    },
+    {
+      "name": "Freeda",
+      "role": "Project Manager",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["azure", "orange"]
+    },
+    {
+      "name": "Agustin",
+      "role": "UX Designer",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["white", "gold"]
+    },
+    {
+      "name": "Logan",
+      "role": "Project Manager",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["azure", "orange"]
+    },
+    {
+      "name": "Raegan",
+      "role": "Strategist",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["fuchsia", "grey"]
+    },
+    {
+      "name": "Karina",
+      "role": "User Researcher",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["plum", "orange"]
+    },
+    {
+      "name": "Amos",
+      "role": "Creative Director",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["salmon", "ivory"]
+    },
+    {
+      "name": "Kennith",
+      "role": "Strategist",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["ivory", "teal"]
+    },
+    {
+      "name": "Kieran",
+      "role": "Visual Designer",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["white", "violet"]
+    },
+    {
+      "name": "Kari",
+      "role": "Creative Director",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["mint green", "violet"]
+    },
+    {
+      "name": "Geovany",
+      "role": "Interaction Design",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["silver", "silver"]
+    },
+    {
+      "name": "Carlo",
+      "role": "User Researcher",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["gold", "pink"]
+    },
+    {
+      "name": "Dolores",
+      "role": "Project Manager",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["indigo", "azure"]
+    },
+    {
+      "name": "Pattie",
+      "role": "Visual Designer",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["magenta", "violet"]
+    },
+    {
+      "name": "Edythe",
+      "role": "Visual Designer",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["salmon", "white"]
+    },
+    {
+      "name": "Marilou",
+      "role": "Project Manager",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["olive", "tan"]
+    },
+    {
+      "name": "Devyn",
+      "role": "Project Manager",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["red", "pink"]
+    },
+    {
+      "name": "Wilbert",
+      "role": "Project Manager",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["teal", "turquoise"]
+    },
+    {
+      "name": "Rosemarie",
+      "role": "Strategist",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["fuchsia", "mint green"]
+    },
+    {
+      "name": "Golda",
+      "role": "Creative Director",
+      "location": "berlin",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["azure", "lavender"]
+    },
+    {
+      "name": "Pete",
+      "role": "User Researcher",
+      "location": "amsterdam",
+      "image": "https://loremflickr.com/640/480/cat",
+      "tags": ["grey", "lavender"]
+    }
+  ]
+  
