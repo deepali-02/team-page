bed708a41d9d5b3a145c900a7d22a1bb20e7d808
deepali-02 <deepali93.redekar@gmail.com>
2022-04-30 15:54:16 +0200

    Created title for the page and added css

--- src/Page/Team.js
+++ src/Page/Team.js
@@ -1,5 +1,21 @@
-export const Team = ()=>{
-return(<div>
-    <h1>TEAM</h1>
-</div>)
-}
+import "./style.css";
+export const Team = () => {
+  return (
+    <div>
+      <div className="title-container" id="grad1">
+        <svg
+          height="150"
+          stroke="#000"
+          stroke-width="2"
+          class="text-line"
+          width="100%"
+        >
+          <text  x="50%" dominant-baseline="middle" text-anchor="middle" y="50%">
+            Meet our team
+          </text>
+        </svg>
+      </div>
+      <div className="body-container"></div>
+    </div>
+  );
+};
