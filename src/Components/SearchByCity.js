ee00038c1c6cd7e38606b2b1713b159898591098
deepali-02 <deepali93.redekar@gmail.com>
2022-05-01 20:13:29 +0200

    added buttons to select city

--- src/Components/SearchByCity.js
+++ src/Components/SearchByCity.js
@@ -1,27 +1,37 @@
 import "./style.css";
 
-
 export const SearchByCity = () => {
+  const handleClick = (e) => {
+    console.log(e.target.value);
+  };
   return (
-    <div>
-      <ul className="showcase">
-        <li>
-         
-          <button>All</button>
-        </li>
+    <>
+      <div>
+        <ul className="showcase">
+          <li>
+            <button value="all" onClick={handleClick}>
+              All
+            </button>
+          </li>
 
-        <li>
-          <button>Berline</button>
-        </li>
+          <li>
+            <button value="berlin" onClick={handleClick}>
+              Berlin
+            </button>
+          </li>
 
-        <li>
-        
-          <button>Amsterdam</button>
-        </li>
-        <li>
-          <button>Cape town</button>
-        </li>
-      </ul>
-    </div>
+          <li>
+            <button value="amsterdam" name="amsterdam" onClick={handleClick}>
+              Amsterdam
+            </button>
+          </li>
+          <li>
+            <button value="cape town" onClick={handleClick}>
+              Cape town
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
