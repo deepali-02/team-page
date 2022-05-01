95234dd70e12e95b2036227cbfe317bb124d76c7
deepali-02 <deepali93.redekar@gmail.com>
2022-05-01 20:24:09 +0200

    wrote handleClick function to get data as per selected city

--- src/Components/SearchByCity.js
+++ src/Components/SearchByCity.js
@@ -1,8 +1,24 @@
 import "./style.css";
+import { team } from "../Data/team";
+import { useState } from "react";
 
 export const SearchByCity = () => {
+    const [teamByCity, setTeamByCity] = useState(team);
   const handleClick = (e) => {
     console.log(e.target.value);
+    if (e.target.value === "berlin") {
+        setTeamByCity(team.filter((t) => t.location === "berlin"));
+      }
+      if (e.target.value === "amsterdam") {
+        setTeamByCity(team.filter((t) => t.location === "amsterdam"));
+      }
+      if (e.target.value === "cape town") {
+        setTeamByCity(team.filter((t) => t.location === "cape town"));
+      }
+      if (e.target.value === "all") {
+        setTeamByCity(team);
+      }
+
   };
   return (
     <>
