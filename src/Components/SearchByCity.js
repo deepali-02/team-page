d0c6ae6418144f43ccf00c9055c479a98e6829fd
deepali-02 <deepali93.redekar@gmail.com>
2022-05-01 12:28:25 +0200

    create new component SearchByCity

---
+++ src/Components/SearchByCity.js
@@ -0,0 +1,20 @@
+export const SearchByCity = ()=>{
+    return(
+        <div className="container">
+        <div className="row">
+          <div className="column">
+            <h3>All</h3>
+          </div>
+          <div className="column">
+            <h3>Berline</h3>
+          </div>
+          <div className="column">
+            <h3>Amsterdam</h3>
+          </div>
+          <div className="column">
+            <h3>Cape town</h3>
+          </div>
+        </div>
+      </div>
+    )
+}
